0064b9b2247814cc0c9ac210629ed0321bbde6e7
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-01 23:05:03 +0000

    lesson: basic hello world with formatted time in time zone.

--- packages/basics/main.go
+++ packages/basics/main.go
@@ -1,5 +1,19 @@
 // packages/basics/main.go
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	fmt.Println("Hello, World from Gopher!")
+	tz, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(
+		"The date and time now, in Tokyo, is:",
+		time.Now().In(tz).Format(time.RFC1123),
+	)
 }
