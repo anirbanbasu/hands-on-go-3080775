e64f0d3c2873e404c3414e89d76361842b4bdfcf
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-06 13:34:37 +0000

    feat: Finished the packages challenge (challenge 1). note: Used a multi-valued return function to return both the saying and the link for a proverb.

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -1,10 +1,24 @@
 // challenges/packages/begin/proverbs.go
 package main
 
+import (
+	"fmt"
+
+	"github.com/jboursiquot/go-proverbs"
+)
+
 // import the proverbs package
 
 // getRandomProverb returns a random proverb from the proverbs package
+func getRandomProverb() (saying, link string) {
+	// call the appropriate function from the proverbs package to get a random proverb
+	randomProverb := proverbs.Random()
+	saying = randomProverb.Saying
+	link = randomProverb.Link
+	return
+}
 
 func main() {
 	// print the result of calling your getRandomProverb function
+	fmt.Println(getRandomProverb())
 }
