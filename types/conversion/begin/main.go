95a3ad960898abd1c65d6c68414f6d63c3e6f06b
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-12 13:46:57 +0000

    chore: update type conversion example to show uint64 conversion and negative float handling

--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,17 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare float and convert it to an unsigned int
+	a := -42.575
+	b := uint64(a)
+
+	fmt.Printf("%v=%T %v=%T\n", a, a, b, b)
+
+	a = 42.575
+	b = uint64(a)
+
+	fmt.Printf("%v=%T %v=%T\n", a, a, b, b)
 }
