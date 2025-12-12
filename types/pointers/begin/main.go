015112125cac6b2a223eab02eca63fe74e55b366
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-12 13:53:13 +0000

    chore: complete pointers begin example

--- types/pointers/begin/main.go
+++ types/pointers/begin/main.go
@@ -1,14 +1,23 @@
 // types/pointers/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// create a variable of type *T where T is an int
+	var a *int
 
 	// declare and assign `b` variable of type int
+	var b int = 42
 
 	// assign the address of b to a
+	a = &b
 
 	// print out the value of a which is the address of b
+	fmt.Printf("address: %p\n", a)
 
 	// print out the value at the address of b
+	fmt.Printf("value: %d\n", *a)
+
+	fmt.Printf("%d [%p]\n", *a, a)
 }
