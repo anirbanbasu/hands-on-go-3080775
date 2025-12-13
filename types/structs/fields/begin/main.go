45312d432ac26ed5d931d004cae16859fdf02ade
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-13 08:54:40 +0000

    chore: completed a basic struct definition, initialisation and printing.

--- types/structs/fields/begin/main.go
+++ types/structs/fields/begin/main.go
@@ -1,13 +1,24 @@
 // types/structs/fields/begin/main.go
 package main
 
+import "fmt"
+
 // define a struct type for author
-//
+type author struct {
+	firstName string
+	lastName  string
+	age       int
+}
 
 func main() {
 	// intialize author
 	//
+	a := author{
+		firstName: "John",
+		lastName:  "Doe",
+		age:       30,
+	}
 
 	// print the author
-	// fmt.Printf("%#v\n", a)
+	fmt.Printf("%#v\n", a)
 }
