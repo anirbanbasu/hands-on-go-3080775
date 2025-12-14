90331683328ac486d6ddf357c740ed127daf6c9f
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-14 08:20:40 +0000

    chore: Completed loop exercises.

--- flow-control/loop-basic/begin/main.go
+++ flow-control/loop-basic/begin/main.go
@@ -1,10 +1,16 @@
 // flow-control/loop-basic/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare a string to iterate over
-	//
+	s := "Hello world"
 
 	// iterate over the string with basic for loop
 	//
+	for i := 0; i < len(s); i++ {
+		// print each character
+		fmt.Println(i, ":", string(s[i]))
+	}
 }
--- flow-control/loop-range/begin/main.go
+++ flow-control/loop-range/begin/main.go
@@ -1,16 +1,31 @@
 // flow-control/loop-range/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// initialize a slice of ints
-	//
+	nums := []int{10, 20, 30, 40, 50}
 
 	// use for-range to iterate over the slice and print each value
-	//
+	for idx, num := range nums {
+		fmt.Println(idx, ":", num)
+	}
 
 	// declare a map of strings to ints
 	//
+	m := map[string]int{
+		"apple":   5,
+		"banana":  10,
+		"cherry":  15,
+		"avocado": 20,
+		"date":    25,
+		"fig":     30,
+	}
 
 	// use for-range to iterate over the map and print each key/value pair
 	//
+	for key, value := range m {
+		fmt.Println(key, ":", value)
+	}
 }
