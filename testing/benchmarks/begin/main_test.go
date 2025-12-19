2d2afcf84d4446290e150a3813e6acda09fb59b9
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-19 14:10:51 +0000

    chore: Completed benchmark exercise.

--- testing/benchmarks/begin/main_test.go
+++ testing/benchmarks/begin/main_test.go
@@ -1,6 +1,32 @@
 // testing/benchmarks/begin/main_test.go
 package main
 
+import "testing"
+
 // write a benchmark for sum
+func BenchmarkSum(b *testing.B) {
+	numbers := make([]int, 1000)
+	for i := 0; i < 1000; i++ {
+		numbers[i] = i
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sum(numbers...)
+	}
+}
 
 // write a benchmark for sumAny
+func BenchmarkSumAny(b *testing.B) {
+	numbers := make([]int, 1000)
+	for i := 0; i < 1000; i++ {
+		numbers[i] = i
+	}
+	b.ResetTimer()
+	interfaces := make([]interface{}, len(numbers))
+	for i, v := range numbers {
+		interfaces[i] = v
+	}
+	for i := 0; i < b.N; i++ {
+		sumAny(interfaces)
+	}
+}
