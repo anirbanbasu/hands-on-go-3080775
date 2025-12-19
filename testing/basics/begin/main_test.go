85e41f7be808e362aa737ed2947d59a44372b6d3
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-19 13:46:12 +0000

    chore: completed basic testing example.

--- testing/basics/begin/main_test.go
+++ testing/basics/begin/main_test.go
@@ -1,6 +1,26 @@
 // testing/basics/begin/main_test.go
 package main
 
+import "testing"
+
 // write a test for sum
+func TestSum(t *testing.T) {
+	got := sum(1, 2, 3, 4, 5)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
 
 // write a TestMain for setup and teardown
+func TestMain(m *testing.M) {
+	// setup code here
+	println("Setting up tests...")
+
+	// run tests
+	m.Run()
+
+	// teardown code here
+	println("Tearing down tests...")
+}
