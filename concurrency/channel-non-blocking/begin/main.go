bad061953c6467f2b3dbc94a65b430c812269305
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-12-21 12:54:04 +0000

    chore: Completed most channel exercises except with sync.

--- concurrency/channel-non-blocking/begin/main.go
+++ concurrency/channel-non-blocking/begin/main.go
@@ -1,8 +1,33 @@
 // concurrency/channel-non-blocking/begin/main.go
 package main
 
+import "time"
+
 func main() {
 	// declare a signal channel to read from
+	timeChan1 := time.After(100 * time.Millisecond)
+	timeChan2 := time.After(200 * time.Millisecond)
+	timeChan3 := time.After(300 * time.Millisecond)
+	timeChan4 := time.After(400 * time.Millisecond)
 
 	// use a default case in select to prevent blocking
+	for {
+		select {
+		case <-timeChan1:
+			println("Received signal from timeChan1")
+			return
+		case <-timeChan2:
+			println("Received signal from timeChan2")
+			return
+		case <-timeChan3:
+			println("Received signal from timeChan3")
+			return
+		case <-timeChan4:
+			println("Received signal from timeChan4")
+			return
+		default:
+			println("No signals received yet")
+			time.Sleep(250 * time.Millisecond)
+		}
+	}
 }
--- concurrency/channel-select/begin/main.go
+++ concurrency/channel-select/begin/main.go
@@ -1,13 +1,27 @@
 // concurrency/channel-select/begin/main.go
 package main
 
+import "time"
+
 func main() {
 	// declare an empty struct channel for signaling
+	signalChan := make(chan struct{})
 
 	// declare a timer channel
+	timerChan := time.After(2 * time.Second)
 
 	// launch a goroutine to signal after 1 second
+	go func() {
+		time.Sleep(1 * time.Second)
+		signalChan <- struct{}{}
+	}()
 
 	// wait for a signal on either channel
 	//
+	select {
+	case <-signalChan:
+		println("Received signal from signalChan")
+	case <-timerChan:
+		println("Received signal from timerChan")
+	}
 }
